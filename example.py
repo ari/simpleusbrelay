bdd862cf4c75ef683a19cc6447fc09b0f141e4bd
Ari Maniatis <ari@ish.com.au>
2014-09-23 20:29:17 +1000

    Parameters don't match function definition

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ import time
 import simpleusbrelay
 
 # initialize the library with the idVendor and Product id
-relaycontroller=simpleusbrelay(idVendor=16c0, idProduct=05df)
+relaycontroller=simpleusbrelay(Vendor = 0x16c0, Product = 0x05df)
 #turn on relay 1
 relaycontroller.array_on(1)
 #turn of relay 1
